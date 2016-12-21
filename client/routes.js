2b6cd7bf03085dec098641bd8a9b7c04007bf0c2
Sam Houston <shouston3@me.com>
2016-12-21 00:21:47 +0000

    Removing dumb components that just wrap up children and replace them with a one line stateless function

--- client/routes.js
+++ client/routes.js
@@ -3,25 +3,25 @@ import { Route, IndexRedirect } from 'react-router';
 
 import authenticate from './authenticate.js';
 
-import App from './containers/index.js';
 import Demo from './containers/Demo.js';
 import About from './containers/About.js';
 import Login from './containers/Login.js';
 import Register from './containers/Register.js';
-import Home from './containers/Home/index.js';
 import Dashboard from './containers/Home/Dashboard.js';
 import Edit from './containers/Home/Edit.js';
 import Practice from './containers/Home/Practice.js';
 import Settings from './containers/Home/Settings.js';
 
+const wrapper = ({ children }) => <div> { children } </div>;
+
 export default (
-  <Route path='/' component={ App }>
+  <Route path='/' component={ wrapper }>
     <IndexRedirect to='/demo' />
     <Route path='demo' component={ Demo } />
     <Route path='about' component={ About } />
     <Route path='login' component={ Login } />
     <Route path='register' component={ Register } />
-    <Route path='home' component={ Home } onEnter={ authenticate }>
+    <Route path='home' component={ wrapper } onEnter={ authenticate }>
       <IndexRedirect to='/dashboard' />
       <Route path='dashboard' component={ Dashboard } />
       <Route path='edit' component={ Edit } />
